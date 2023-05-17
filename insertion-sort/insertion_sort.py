e2f0a40975f441ca8469a1eb28cb6510e54dc577
Oliver Beresford <oggynaut@icloud.com>
2023-05-17 19:35:27 -0400

    Finished insertion_sort, now fully works

---
+++ insertion-sort/insertion_sort.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def insertion_sort(array: np.array) -> np.array:
+    for i in range(len(array)):
+        value = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > value:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = value
+    return array
+
+print(insertion_sort(np.array([8, 1, 8, 0, 9, 4, 1, 2, 3])))
