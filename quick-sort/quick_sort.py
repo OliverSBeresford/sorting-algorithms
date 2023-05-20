e4535f82b8eff8f74203a8ee1fb9372ba886e479
Oliver Beresford <oggynaut@icloud.com>
2023-05-20 11:25:16 -0400

    Quicksort function now chooses a pivot and puts all numbers smaller to it on the left and all numbers bigger than it on the right. This is the first step in the process.

---
+++ quick-sort/quick_sort.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def quicksort(array: np.ndarray, length) -> np.ndarray:
+    if length < 2:
+        return array
+
+    pivot = array[length - 1]
+
+    i = 0
+    j = length - 2
+
+    while True:
+        while array[i] < pivot:
+            i += 1
+        while array[j] > pivot:
+            j -= 1 
+        if i < j:
+            array[j], array[i] = array[i], array[j]
+            i += 1
+            j -= 1
+        else:
+            break
+    
+    array[length -1], array[i] = array[i], array[length - 1]
+
+    return array
+
+print(quicksort(np.array([5, 2, 1, 1.5, 6, 4, 3]), 7))
