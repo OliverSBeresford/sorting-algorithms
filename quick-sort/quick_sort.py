77126b2541897bd8bb75332a7f5e53e35b551689
Oliver Beresford <oggynaut@icloud.com>
2023-05-28 21:44:12 -0400

    Changed the function name to match the others. Although it is not entirely correct, it makes it easier to implement these in an external code (based on the knowledge of the other files).

--- quick-sort/quick_sort.py
+++ quick-sort/quick_sort.py
@@ -21,11 +21,11 @@ def partition(array: np.ndarray, start: int, end: int) -> int:
         
         array[i], array[j] = array[j], array[i]
 
-def quicksort(array, start, end):
+def quick_sort(array, start, end):
     if start < end:
         pivot_index = partition(array, start, end)
-        quicksort(array, start, pivot_index)
-        quicksort(array, pivot_index + 1, end)
+        quick_sort(array, start, pivot_index)
+        quick_sort(array, pivot_index + 1, end)
 
 userinput = input("Enter the array you want to sort with each element seperated by spaces:\n").split()
 try:
@@ -33,5 +33,5 @@ try:
 except:
     arr = np.array([x for x in userinput], dtype=str)
 
-quicksort(arr, 0, len(userinput) - 1)
+quick_sort(arr, 0, len(userinput) - 1)
 print("{", *arr, "}")
